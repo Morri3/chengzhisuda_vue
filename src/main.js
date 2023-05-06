8cd99559d5ea8c65ecad301fc87613516a3a0d76
Morri3 <824440291@qq.com>
2023-05-06 10:52:17 +0800

    fix:添加main.js代码注释

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import 'element-plus/dist/index.css'// 导入elementPlus
 import '@/styles/index.scss'// 引入字体
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'// 导入icon图标
 import axios from 'axios' // axios导入
-import moment from 'moment'
-import * as echarts from 'echarts'
+import moment from 'moment' // 导入js时间类库
+import * as echarts from 'echarts' // 导入echarts
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
@@ -17,9 +17,9 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 }
 
 // axios的配置
-app.config.globalProperties.$axios = axios // 配置axios的全局引用
-app.config.globalProperties.$moment = moment
-app.config.globalProperties.$echarts = echarts
+app.config.globalProperties.$axios = axios // 配置axios全局引用
+app.config.globalProperties.$moment = moment // 配置js时间类库全局引用
+app.config.globalProperties.$echarts = echarts // 配置echarts全局引用
 // axios.defaults.baseURL = '/api'
 // // axios.defaults.baseURL = 'http://114.55.239.213:8087/'
 // axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
